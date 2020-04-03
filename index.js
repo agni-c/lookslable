3188e5871900025887b428c37c7e16776cbdab89
agni.c <agnibha0016@gmail.com>
2020-04-03 16:27:45 +0530

    uploads data  to firestore

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-const express = require("express");
-const Datastore = require("nedb");
-
+const express = require('express');
+const path = require('path');
 const app = express();
+const firebase = require('@google-cloud/firestore');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+//Firestore init--------------------------
 
-require("dotenv").config();
-const port = process.env.PORT || 443;
+var admin = require('firebase-admin');
 
+var serviceAccount = require('./spring-internship-firebase-adminsdk-7z0b1-5e95103165.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://spring-internship.firebaseio.com'
+});
+
+let db = admin.firestore();
+
+//----------------------------------------
+
+app.use(express.static('public')); //only assets in public will be recognized
+app.use(express.json()); //parsing json, limiting it to 1mb
+
+//Setting doc reference
+let docRef = db.collection('test');
+//get request
+app.get('/api', (request, response) => {
+	//get data from firebase
+	//Reading the data
+	db.collection('test')
+		.get()
+		.then(snapshot => {
+			snapshot.forEach(doc => {
+				response.json(doc.id);
+			});
+		})
+		.catch(err => {
+			console.log('Error getting documents', err);
+		});
+});
+
+//post request
+app.post('/api', (request, response) => {
+	const data = request.body; //saving req in data
+	const timestamp = firebase.FieldValue.serverTimestamp(); //saving current date
+	data.timestamp = timestamp; //saving current time var in data obj
+	// response.json(data); //sending response to debug
+
+	//test firebase post
+	docRef.add(data); // can send to firestore
+});
+
+//Listening
 app.listen(port, () => {
-  console.log(`listening at ${port}`);
-});
-
-app.use(express.static("public"));
-app.use(express.json({ limit: "1mb" }));
-
-const database = new Datastore("database.db");
-database.loadDatabase();
-
-app.get("/api", (request, response) => {
-  database.find({}, (err, data) => {
-    if (err) {
-      response.end();
-      return;
-    }
-    response.json(data);
-  });
-});
-
-app.post("/api", (request, response) => {
-  const data = request.body;
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
-  database.insert(data);
-  response.json(data);
+	console.log(`listening at ${port}`);
 });
--- public/logs/logs.js
+++ public/logs/logs.js
@@ -1,50 +1,50 @@
 getData();
 
 const selfies = [];
-
+//sorting data by time
 document.getElementById('time').addEventListener('click', event => {
-  sortData((a, b) => b.time - a.time);
+	sortData((a, b) => b.time - a.time);
 });
-
+//sorting data by landmark
 document.getElementById('landmark').addEventListener('click', event => {
-  sortData((a, b) => {
-    if (b.landmark > a.landmark) return -1;
-    else return 1;
-  });
+	sortData((a, b) => {
+		if (b.landmark > a.landmark) return -1;
+		else return 1;
+	});
 });
-
+//value from event listener goes to this function (callback -> 'compare' parameter)
 function sortData(compare) {
-  for (let item of selfies) {
-    item.elt.remove();
-  }
-  selfies.sort(compare);
-  for (let item of selfies) {
-    document.body.append(item.elt);
-  }
+	for (let item of selfies) {
+		item.elt.remove();
+	}
+	selfies.sort(compare);
+	for (let item of selfies) {
+		document.body.append(item.elt);
+	}
 }
 
 async function getData() {
-  const response = await fetch('/api');
-  const data = await response.json();
+	const response = await fetch('/api');
+	const data = await response.json();
 
-  for (item of data) {
-    const root = document.createElement('p');
-    const landmark = document.createElement('div');
-    const geo = document.createElement('div');
-    const date = document.createElement('div');
-    const image = document.createElement('img');
+	for (item of data) {
+		const root = document.createElement('p');
+		const landmark = document.createElement('div');
+		const geo = document.createElement('div');
+		const date = document.createElement('div');
+		const image = document.createElement('img');
 
-    landmark.textContent = `landmark: ${item.landmark}`;
-    geo.textContent = `${item.lat}°, ${item.lon}°`;
-    const dateString = new Date(item.timestamp).toLocaleString();
-    date.textContent = dateString;
-    image.src = item.image64;
-    image.alt = 'Dan Shiffman making silly faces.';
+		landmark.textContent = `landmark: ${item.landmark}`;
+		geo.textContent = `${item.lat}°, ${item.lon}°`;
+		const dateString = new Date(item.timestamp).toLocaleString();
+		date.textContent = dateString;
+		image.src = item.image64;
+		image.alt = 'Dan Shiffman making silly faces.';
 
-    root.append(landmark, geo, date, image);
+		root.append(landmark, geo, date, image);
 
-    selfies.push({ elt: root, time: item.timestamp, landmark: item.landmark });
-    document.body.append(root);
-  }
-  console.log(data);
+		selfies.push({ elt: root, time: item.timestamp, landmark: item.landmark });
+		document.body.append(root);
+	}
+	console.log(data);
 }
