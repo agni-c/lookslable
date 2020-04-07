7bbb36a2a416b1687283cc3e93a52d17b288baaf
agni.c <agnibha0016@gmail.com>
2020-04-08 01:33:34 +0530

    Images gets to cloud storage now

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const firebase = require('@google-cloud/firestore');
+const { Storage } = require('@google-cloud/storage');
+const { v4 } = require('uuid');
+const fs = require('fs');
+//--------------------------------------
+const stream = require('stream');
+const bufferStream = new stream.PassThrough();
+//--------------------------------------
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 //Firestore init--------------------------
@@ -12,10 +19,13 @@ var serviceAccount = require('./spring-internship-firebase-adminsdk-7z0b1-ad7d9b
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
-	databaseURL: 'https://spring-internship.firebaseio.com',
+	databaseURL: process.env.FIREBASE_DB,
+	storageBucket: process.env.BUCKET_NAME,
 });
 
-let db = admin.firestore();
+const db = admin.firestore();
+const storage = new Storage();
+const bucket = storage.bucket(process.env.BUCKET_NAME);
 
 //----------------------------------------
 
@@ -44,13 +54,52 @@ app.get('/api', (request, response) => {
 
 //post request
 app.post('/api', (request, response) => {
-	const data = request.body; //saving req in data
-	const timestamp = firebase.FieldValue.serverTimestamp(); //saving current date
-	data.timestamp = timestamp; //saving current time var in data obj
-	// response.json(data); //sending response to debug
-	//test firebase post
+	const data = request.body;
+	const timestamp = firebase.FieldValue.serverTimestamp();
+	data.timestamp = timestamp;
+	data._id = v4().split('-').pop();
+
+	//converting data-url to image
+	let imgString = data.image64;
+	let trimmedImg = imgString.split(';base64,').pop();
+	fs.writeFile(
+		`./temp/${data._id}.png`,
+		trimmedImg,
+		{ encoding: 'base64' },
+		function (err) {
+			console.log('File created');
+		}
+	);
+	//uploading files to firebase
+	let localReadStream = fs.createReadStream(`./temp/${data._id}.png`);
+	let remoteWriteStream = bucket.file(`${data._id}.png`).createWriteStream({
+		metadata: {
+			contentType: 'image/png',
+			metadata: {
+				custom: 'metadata',
+			},
+		},
+	});
+	localReadStream
+		.pipe(remoteWriteStream)
+		.on('error', function (err) {
+			console.log(`Failed to upload: ${err}`);
+		})
+		.on('finish', function () {
+			// The file upload is complete.
+			console.log('upload finished');
+		});
+
+	//setting firestore image attribute to the url
+	data.image64 = `https://storage.googleapis.com/${process.env.BUCKET_NAME}/${data._id}.png`;
+
 	let docRef = db.collection('test');
 	docRef.add(data); // can send to firestore
+	//Deletes the local file
+	fs.unlink(`./temp/${data._id}.png`, (err) => {
+		if (err) throw err;
+		console.log('deleting the local file');
+	});
 });
 
 //Listening
