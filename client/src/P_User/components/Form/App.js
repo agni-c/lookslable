bb8367531a961af2bf24fead370776b5c1bac397
agni.c <agnibha0016@gmail.com>
2020-05-07 12:47:38 +0530

    Basic form added in p_user/components/form

--- client/src/P_User/components/Form/App.js
+++ client/src/P_User/components/Form/App.js
@@ -2,13 +2,68 @@ import React from "react";
 import "./styles.css";
 import Upload from "./Upload";
 import { Container, Button } from "react-bootstrap";
-export default function AppForm() {
-  return (
-    <div className="App">
-      <Container>
-        <h2>Upload Images</h2>
-        <Upload />
-      </Container>
-    </div>
-  );
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<form
+				action='http://localhost:5000/spring-internship/us-central1/app/api/upload'
+				method='post'
+				enctype='multipart/form-data'>
+				<label for='upload'>Upload Images</label>
+				<input
+					type='file'
+					accept='image/*'
+					id='upload'
+					name='uploads'
+					multiple
+					required
+				/>
+				<br />
+				<br />
+
+				<label for='landmark'>Landmark</label>
+				<input
+					type='text'
+					name='landmark'
+					placeholder='eg. lake gardens'
+					required
+					id='landmark'
+				/>
+				<br />
+				<br />
+
+				<label for='price'>Price</label>
+
+				<select id='price' name='price'>
+					<option value='149'>149</option>
+					<option value='249'>249</option>
+					<option value='499'>499</option>
+					<option value='999'>999</option>
+					<option value='999'>999</option>
+					<option value='1499'>1499</option>
+				</select>
+				<br />
+				<br />
+
+				<label for='time'>(date and time)</label>
+				<input type='datetime-local' id='time' name='time' />
+
+				<br />
+				<br />
+
+				<label class='location'>
+					<input type='radio' name='location' value='Indoor' /> Indoor
+					<br />
+					<input type='radio' name='location' value='Outdoor' /> Outdoor
+					<br />
+				</label>
+
+				<br />
+				<br />
+
+				<button type='submit'>Upload</button>
+			</form>
+		);
+	}
 }
