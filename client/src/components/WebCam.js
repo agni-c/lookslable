76df45b094e75768b79a323c45062cac0c86b66d
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-05-11 16:52:54 +0530

    Done some changes to cam module

--- client/src/components/WebCam.js
+++ client/src/components/WebCam.js
@@ -1,35 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import Webcam from "react-webcam";
 import { Button } from "react-bootstrap";
 
-function WebCam() {
-  const videoConstraints = {
+// function WebCam() {
+//   const videoConstraints = {
+//     width: 350,
+//     height: 350,
+//     facingMode: "user",
+//   };
+//   const webcamRef = React.useRef(null);
+//   const capture = React.useCallback(() => {
+//     const imageSrc = webcamRef.current.getScreenshot();
+//   }, [webcamRef]);
+//   console.log(webcamRef);
+
+//   return (
+//     <>
+//       <Webcam
+//         ref="webcam"
+//         audio={false}
+//         height={350}
+//         ref={webcamRef}
+//         screenshotFormat="image/jpeg"
+//         width={350}
+//         videoConstraints={videoConstraints}
+//       />
+//       <br />
+//       <Button variant="outline-success" onClick={capture}>
+//         Capture photo
+//       </Button>
+//     </>
+//   );
+// }
+
+// export default WebCam;
+
+export default class WebCam extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { screenshot: null };
+  }
+  videoConstraints = {
     width: 350,
     height: 350,
     facingMode: "user",
   };
-  const webcamRef = React.useRef(null);
-  const capture = React.useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-  }, [webcamRef]);
-  console.log(webcamRef);
 
-  return (
-    <>
-      <Webcam
-        audio={false}
-        height={350}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        width={350}
-        videoConstraints={videoConstraints}
-      />
-      <br />
-      <Button variant="outline-success" onClick={capture}>
-        Capture photo
-      </Button>
-    </>
-  );
+  screenshot() {
+    var screenshot = this.refs.webcam.getScreenshot();
+    this.setState({ screenshot: screenshot });
+  }
+
+  render() {
+    return (
+      <div>
+        <Webcam
+          audio={false}
+          ref="webcam"
+          height={350}
+          width={350}
+          videoConstraints={this.videoConstraints}
+        />
+        <br />
+        <Button onClick={this.screenshot.bind(this)}>Capture</Button>
+        {this.state.screenshot ? <img src={this.state.screenshot} /> : null}
+      </div>
+    );
+  }
 }
-
-export default WebCam;
