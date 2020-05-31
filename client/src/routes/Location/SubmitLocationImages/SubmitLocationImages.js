dcd3448f669125eee9e3c56ef5d08b0b87f3be03
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-05-31 16:56:15 +0530

    Solved all Routing Problems

--- client/src/routes/Location/SubmitLocationImages/SubmitLocationImages.js
+++ client/src/routes/Location/SubmitLocationImages/SubmitLocationImages.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import App from "../../../components/Forms/Form/App";
-import { useHistory } from "react-router-dom";
 import { Button } from "react-bootstrap";
-const SubmitLocationImages = () => {
-  let history = useHistory();
-  function handleClick() {
-    history.push("/user/location");
+import HeroMain from "../../../P_User/components/HeroMain";
+import Location from "../Location";
+
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Switch,
+  HashRouter,
+  Router,
+} from "react-router-dom";
+class SubmitLocationImages extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/user/location" exact component={Location} />
+
+          <Route path="/user/location/submit-location-images">
+            <div className="backButton">
+              <Link to="/user/location">
+                <Button variant="outline-primary">Go Back</Button>
+              </Link>
+            </div>
+            <App />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
   }
-  return (
-    <>
-      <Button onClick={handleClick}> Go Back</Button>
-      <App />
-    </>
-  );
-};
+}
 
 export default SubmitLocationImages;
