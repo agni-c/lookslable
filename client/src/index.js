99bf2bf9f175b27a93c8390a7fd39e040214432f
agni.c <agnibha0016@gmail.com>
2020-06-01 13:19:14 +0530

    dummy commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import App from "./P_User/components/App";
+import App from "./I_User/components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDom.render(
