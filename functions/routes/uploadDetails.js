b111840840697e9a560baaaf98a1b53f2f52d645
agni.c <agnibha0016@gmail.com>
2020-06-01 13:34:05 +0530

    going to merge

--- functions/routes/uploadDetails.js
+++ functions/routes/uploadDetails.js
@@ -1,27 +1,23 @@
-const router =require('express').Router()
+const router = require("express").Router();
 var firebase = require("firebase");
-var database =require('./firebaseAppI');
- require('dotenv').config();
+var database = require("./firebaseAppI");
+require("dotenv").config();
 
-router.post('/',(req,res)=>{
-    
-      //giving the reference
-      var ref = database.ref("I_USER");
-      //create the object
-      var obj = {
-        Email:req.body.email,
-        Adress:req.body.address,
-        PhoneNo:req.body.phoneno,
-        Event:req.body.event,
-        Date:req.body.date,
-        Time:req.body.time
-      };
-      //push the object
-       var result = ref.push(obj);
-       res.end();
-    
+router.post("/", (req, res) => {
+	//giving the reference
+	var ref = database.ref("MY_LOCATION");
+	//create the object
+	var obj = {
+		Email: req.body.email,
+		Adress: req.body.address,
+		PhoneNo: req.body.phoneno,
+		Event: req.body.event,
+		Date: req.body.date,
+		Time: req.body.time,
+	};
+	//push the object
+	ref.push(obj);
+	res.end();
+});
 
-})
-
-
-module.exports =router
+module.exports = router;
