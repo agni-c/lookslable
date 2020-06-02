ac741de390ff0e0ecc43a62771e0a48031bae594
agni.c <agnibha0016@gmail.com>
2020-06-02 15:28:37 +0530

    card change

--- functions/routes/booking.js
+++ functions/routes/booking.js
@@ -1,20 +1,20 @@
-var router = require('express').Router();
-var firebase = require('firebase');
-var database =require('./firebaseAppI');
-router.post('/',(req,res)=>{
-    console.log("in");
- var ref = database.ref("BOOKING_DETAILS");
- var obj ={
-     Name:req.body.name,
-     Numbersofuser:req.body.numbersofuser,
-     Price:req.body.price,
-     Iuid:'126',
-     Puid:'111',
-     Eventdate:req.body.eventdate,
-     bookingdate: new Date().toISOString().substring(0,10)
-    }
-    ref.push(obj);
-    res.end();
-})
+var router = require("express").Router();
+var firebase = require("firebase");
+var database = require("./firebaseAppI");
+router.post("/:uid", (req, res) => {
+	console.log("in");
+	var ref = database.ref("BOOKING_DETAILS");
+	var obj = {
+		Name: req.body.name,
+		Numbersofuser: req.body.numbersofuser,
+		Price: req.body.price,
+		Iuid: req.params.uid,
+		//  Puid:'111',
+		Eventdate: req.body.eventdate,
+		bookingdate: new Date().toISOString().substring(0, 10),
+	};
+	ref.push(obj);
+	res.end();
+});
 
 module.exports = router;
