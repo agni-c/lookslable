5457146e1d3809213c507f94eccaa91a4aece277
agni.c <agnibha0016@gmail.com>
2020-06-09 13:14:40 +0530

    lol2

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import App from "./P_User/components/App";
+import App from "./I_User/components/App";
 
 ReactDom.render(<App />, document.getElementById("root"));
