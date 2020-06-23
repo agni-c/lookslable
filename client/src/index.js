0b7625672f57701c61d3dd3ec5ddbd24885f950f
Hiraparavishal <vishuhirapara@gmail.com>
2020-06-23 15:53:33 +0530

    working puser

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 ReactDom.render(
   <Router>
     {/* {<Iuser/>} */}
-    <AdminApp />
+    <Iuser />
     {/* <App /> */}
   </Router>,
   document.getElementById('root')
