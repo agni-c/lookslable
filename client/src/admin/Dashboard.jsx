1b6b10d8fd5223bbbba3e0065171e55c4ed78058
agni.c <agnibha0016@gmail.com>
2020-06-23 15:33:56 +0530

    fixed react app

--- client/src/admin/Dashboard.jsx
+++ client/src/admin/Dashboard.jsx
@@ -52,108 +52,107 @@ export default function ClippedDrawer() {
 	const classes = useStyles();
 
 	return (
-		<PuserState>
-			<Router>
-				<div className={classes.root}>
-					<CssBaseline />
-					<AppBar position="fixed" className={classes.appBar}>
-						<Toolbar>
-							<Typography variant="h6" noWrap>
-								Admin Panel
-							</Typography>
-						</Toolbar>
-					</AppBar>
-					<Drawer
-						className={classes.drawer}
-						variant="permanent"
-						classes={{
-							paper: classes.drawerPaper,
-						}}>
-						<Toolbar />
+		<Router>
+			<div className={classes.root}>
+				<CssBaseline />
+				<AppBar position="fixed" className={classes.appBar}>
+					<Toolbar>
+						<Typography variant="h6" noWrap>
+							Admin Panel
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<Drawer
+					className={classes.drawer}
+					variant="permanent"
+					classes={{
+						paper: classes.drawerPaper,
+					}}>
+					<Toolbar />
 
-          <div className={classes.drawerContainer}>
-            <List>
-              <Link to='/dashboard/puser'>
-                <ListItem button>
-                  <ListItemIcon>
-                    <Portrait />
-                  </ListItemIcon>
-                  <ListItemText primary='P user' />
-                </ListItem>
-              </Link>
-            </List>
-            <List>
-              <Link to='/dashboard/iuser'>
-                <ListItem button>
-                  <ListItemIcon>
-                    <Portrait />
-                  </ListItemIcon>
-                  <ListItemText primary='I user' />
-                </ListItem>
-              </Link>
-            </List>
-            <List>
-              <Link to='/dashboard/bookings'>
-                <ListItem button>
-                  <ListItemIcon>
-                    <Portrait />
-                  </ListItemIcon>
-                  <ListItemText primary='Bookings' />
-                </ListItem>
-              </Link>
-            </List>
-            <List>
-              <Link to='/dashboard/landmark'>
-                <ListItem button>
-                  <ListItemIcon>
-                    <Portrait />
-                  </ListItemIcon>
-                  <ListItemText primary='Landmark' />
-                </ListItem>
-              </Link>
-            </List>
-            <List>
-              <Link to='/dashboard/approvedphotos'>
-                <ListItem button>
-                  <ListItemIcon>
-                    <Portrait />
-                  </ListItemIcon>
-                  <ListItemText primary='Approved Photos' />
-                </ListItem>
-              </Link>
-            </List>
-            <Divider />
-          </div>
-        </Drawer>
-        <main className={classes.content} style={{ height: '100vh' }}>
-          <Toolbar />
-          {/* <PuserFragment /> */}
-          {/* <BookingFragment /> */}
-          <Switch>
-            <Route exact path='/dashboard/puser' component={PuserFragment} />
-          </Switch>
-          <Switch>
-            <Route path='/dashboard/iuser' exact component={IuserFragment} />
-          </Switch>
-          <Switch>
-            <Route path='/dashboard/bookings' component={BookingFragment} />
-          </Switch>
-          <Switch>
-            <Route
-              exact
-              path='/dashboard/landmark'
-              component={LandMarkFragment}
-            />
-          </Switch>
-          <Switch>
-            <Route
-              exact
-              path='/dashboard/approvedphotos'
-              component={ApprovedPhotos}
-            />
-          </Switch>
-        </main>
-      </div>
-    </Router>
-  );
+					<div className={classes.drawerContainer}>
+						<List>
+							<Link to="/dashboard/puser">
+								<ListItem button>
+									<ListItemIcon>
+										<Portrait />
+									</ListItemIcon>
+									<ListItemText primary="P user" />
+								</ListItem>
+							</Link>
+						</List>
+						<List>
+							<Link to="/dashboard/iuser">
+								<ListItem button>
+									<ListItemIcon>
+										<Portrait />
+									</ListItemIcon>
+									<ListItemText primary="I user" />
+								</ListItem>
+							</Link>
+						</List>
+						<List>
+							<Link to="/dashboard/bookings">
+								<ListItem button>
+									<ListItemIcon>
+										<Portrait />
+									</ListItemIcon>
+									<ListItemText primary="Bookings" />
+								</ListItem>
+							</Link>
+						</List>
+						<List>
+							<Link to="/dashboard/landmark">
+								<ListItem button>
+									<ListItemIcon>
+										<Portrait />
+									</ListItemIcon>
+									<ListItemText primary="Landmark" />
+								</ListItem>
+							</Link>
+						</List>
+						<List>
+							<Link to="/dashboard/approvedphotos">
+								<ListItem button>
+									<ListItemIcon>
+										<Portrait />
+									</ListItemIcon>
+									<ListItemText primary="Approved Photos" />
+								</ListItem>
+							</Link>
+						</List>
+						<Divider />
+					</div>
+				</Drawer>
+				<main className={classes.content} style={{ height: "100vh" }}>
+					<Toolbar />
+					{/* <PuserFragment /> */}
+					{/* <BookingFragment /> */}
+					<Switch>
+						<Route exact path="/dashboard/puser" component={PuserFragment} />
+					</Switch>
+					<Switch>
+						<Route path="/dashboard/iuser" exact component={IuserFragment} />
+					</Switch>
+					<Switch>
+						<Route path="/dashboard/bookings" component={BookingFragment} />
+					</Switch>
+					<Switch>
+						<Route
+							exact
+							path="/dashboard/landmark"
+							component={LandMarkFragment}
+						/>
+					</Switch>
+					<Switch>
+						<Route
+							exact
+							path="/dashboard/approvedphotos"
+							component={ApprovedPhotos}
+						/>
+					</Switch>
+				</main>
+			</div>
+		</Router>
+	);
 }
