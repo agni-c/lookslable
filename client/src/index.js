062414b4e224f5e401d84004e454ad9c6d441b20
Hiraparavishal <vishuhirapara@gmail.com>
2020-06-24 11:08:28 +0530

    revert

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-// import App from './rating/App';
-import App from './App';
+import App from './rating/App';
+import AdminApp from './admin/App';
+import Iuser from './P_User/components/App';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDom.render(
   <Router>
-    <App />
+    {/* {<Iuser/>} */}
+    <Iuser />
+    {/* <App /> */}
   </Router>,
   document.getElementById('root')
 );
