e419042161af4db0150379188b77f5790a085c8d
Hiraparavishal <vishuhirapara@gmail.com>
2020-08-04 14:55:43 +0530

    all route of custom booking done

--- functions/routes/uploadlink.js
+++ functions/routes/uploadlink.js
@@ -32,4 +32,33 @@ router.put("/", (req, res) => {
       }
     );
 });
+router.post("/custom", (req, res) => {
+  var ref = database.ref("CUSTOM_BOOKING");
+  ref
+    .orderByChild("puid")
+    .equalTo(req.body.puid)
+    .on(
+      "value",
+      (snapshot) => {
+        var data = Object.values(snapshot.val());
+        var key = Object.keys(snapshot.val());
+        console.log(data);
+        console.log(key);
+        for (var i = 0; i < data.length; i++) {
+          if (
+            data[i].date === req.body.date &&
+            data[i].iuid === req.body.iuid
+          ) {
+            database.ref("CUSTOM_BOOKING/" + key[i]).update({
+              driveLink: req.body.driveLink,
+            });
+            res.end("changed");
+          }
+        }
+      },
+      (errorObject) => {
+        console.log("The read failed: " + errorObject.code);
+      }
+    );
+});
 module.exports = router;
